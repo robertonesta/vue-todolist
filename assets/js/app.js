9be20fa4c8f8223b58f598f1f26eb614cb5658b2
RobertoNesta <robertonesta1993@gmail.com>
2023-03-30 14:24:07 +0200

    initial commit + first instruction

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+/*Descrizione:
+Rifare l'esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+-"text", una stringa che indica il testo del todo;
+-"done", un booleano (true/false) che indica se il todo è stato fatto oppure no.*/
+  const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            
+        }
+    }
+}).mount('#app')
+
